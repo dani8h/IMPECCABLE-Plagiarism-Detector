08176611949f18644117fb425c5c3a190d354f74
Anas Danish <mohammedanasdanish41@gmail.com>
2022-10-17 23:18:01 +0530

    added testcase

---
+++ testcases.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<time.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<stdbool.h>
+#include<ctype.h>
+#include<limits.h>
+
+
+void testcase_1(){
+    if(true)    printf('testcase passed');
+    else printf('not passed');
+}
+
+
+void function add(int a , int b ){
+    return a+b;
+}
+
